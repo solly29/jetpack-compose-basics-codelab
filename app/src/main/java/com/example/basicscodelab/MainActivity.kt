082deaa5560d70b84ef73d16383f2644000ce954
solly29 <cksdud1311@naver.com>
2022-11-12 22:19:39 +0900

    jetpack compose 기초 - UI조정

---
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.basicscodelab
+
+import android.os.Bundle
+import android.view.Surface
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicsCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.primary
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Text(
+            text = "Hello $name!",
+            modifier = Modifier.padding(24.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true, name = "Text Preview")
+@Composable
+fun DefaultPreview() {
+    BasicsCodelabTheme {
+        Greeting("Android")
+    }
+}
